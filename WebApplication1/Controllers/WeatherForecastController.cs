4e0b7fe1525773ffc699c2bceb086c28f585e850
NohGyuWon <NohGyuWon@gmail.com>
2023-05-15 23:17:28 +0900

    rider 기본기능 테스트

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -30,5 +30,34 @@ public class WeatherForecastController : ControllerBase
             .ToArray();
     }
     
+    [HttpGet("{date}", Name = "GetWeatherForecastByDate")]
+    public WeatherForecast Get(DateOnly date)
+    {
+        return new WeatherForecast
+        {
+            Date = date,
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        };
+    }
+    
+    [HttpPost(Name = "CreateWeatherForecast")]
+    public IActionResult Create([FromBody] WeatherForecast weatherForecast)
+    {
+        return CreatedAtRoute("GetWeatherForecastByDate", new {date = weatherForecast.Date}, weatherForecast);
+    }
+    
+    [HttpPut("{date}", Name = "UpdateWeatherForecast")]
+    public IActionResult Update(DateOnly date, [FromBody] WeatherForecast weatherForecast)
+    {
+        return NoContent();
+    }
+    
+    [HttpDelete("{date}", Name = "DeleteWeatherForecast")]
+    public IActionResult Delete(DateOnly date)
+    {
+        return NoContent();
+    }
+    
     
 }
